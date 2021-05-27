d51f0428a2838b8bbfc5f850be152409b522bcf4
Caldremch <caldremch@163.com>
2021-05-28 00:11:32 +0800

    update

--- src/main/kotlin/com/caldremch/leetcode/2_两数相加.kt
+++ src/main/kotlin/com/caldremch/leetcode/2_两数相加.kt
@@ -1,5 +1,7 @@
 package com.caldremch.leetcode
 
+import kotlin.math.abs
+
 /**
  *
  * @auther Caldremch
@@ -34,12 +36,56 @@ class ListNode(var `val`: Int) {
 }
 
 class Solution2 {
-    fun addTwoNumbers(l1: ListNode, l2: ListNode): ListNode? {
+
+    fun addTwoNumbers2(l1: ListNode, l2: ListNode): ListNode? {
+        /**
+         * 1.遍历L1链表 等到size， L2 同理等 size
+         * 2.拼接 L2，形成一条链表
+         * 3.跳步的形式
+         */
+        var next:ListNode? = l1
+        var count1 = 0
+        //n
+        while(next != null){
+            count1++
+            next = next.next
+        }
+
+        var count2 = 0
+        next = l2
+        //n
+        while(next != null){
+            count2++
+            next = next.next
+        }
+
+        l1.next = l2
+        var count3 = 0
+
+
+        val deltaCount=  if (count1>count2){
+            count1-count2
+        }else{
+            count2-count1
+        }
+
+        next = l1
+        while(next != null){
+            //l1 补 0
+            if (count1>count2){
+
+            }
+            count3++
+            next = next.next
+        }
+
+    }
+
+        fun addTwoNumbers(l1: ListNode, l2: ListNode): ListNode? {
         val slot1 = arrayListOf<Int>()
         val slot2 = arrayListOf<Int>()
         var next1: ListNode? = l1
         var next2: ListNode? = l2
-
 
         while (true) {
             if (next1 != null && next2 != null) {
@@ -54,16 +100,18 @@ class Solution2 {
                 next2 = next2.next
                 slot1.add(0, 0)
             } else {
-
                 if (slot1.size == slot2.size) {
                     var rootNode:ListNode? = null
-                    var connectNode:ListNode? = null
+                    var firstNode:ListNode? = null
                     for (x in slot1.size-1 downTo  0){
-
-                        val temp = rootNode?.next
-                        temp?.next = ListNode(slot2[x]+slot1[x])
-                        temp?.next = temp
-
+                        if(rootNode== null){
+                            rootNode = ListNode(slot2[x]+slot1[x])
+                            firstNode = rootNode
+                        }else{
+                            val temp = ListNode(slot2[x]+slot1[x])
+                            firstNode!!.next = temp
+                            firstNode = temp
+                        }
                     }
 
                     return rootNode
